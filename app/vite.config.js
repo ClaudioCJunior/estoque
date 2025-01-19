53fd296418c4872f2384ceace1f7030555275d99
Claud <claudio.cruz@esig.com.br>
2025-01-18 23:41:29 -0300

    Adiciona correções no docker-compose para rodar no modo desenvolvedor

--- app/vite.config.js
+++ app/vite.config.js
@@ -17,4 +17,15 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Permitir acesso externo
+        port: 3000,      // Porta padrão do Vite
+        hmr: {
+            host: 'localhost', // Substitua por 'vite' se você usar um serviço chamado 'vite' no Docker
+            protocol: 'ws',
+        },
+        watch: {
+            usePolling: true, // Necessário para sincronização em sistemas como Docker
+        },
+    },
 });
